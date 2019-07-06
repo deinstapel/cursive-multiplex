3573d0a626b6400e518dac27c34081d57cdfcb5a
Fin Christensen <christensen.fin@gmail.com>
2019-07-06 15:54:12 +0200

    Add CI configuration with shields endpoint

--- tests/end2end.rs
+++ tests/end2end.rs
@@ -1,12 +1,40 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
+use std::fs;
+use serde_json::{json, Value};
 
 #[test]
 fn test_end2end() {
-    assert!(
-        Command::new("./node_modules/.bin/jest")
-            .current_dir("./tests")
-            .status()
-            .expect("end-to-end tests failed")
-            .success()
-    );
+    let jest = Command::new("./node_modules/.bin/jest")
+        .arg("--color")
+        .arg("--json")
+        .current_dir("./tests")
+        .stderr(Stdio::inherit())
+        .output()
+        .expect("end2end tests failed");
+
+    assert!(jest.status.success());
+
+    let result: Value = serde_json::from_slice(&jest.stdout[..])
+        .expect("failed to read end2end tests result");
+    let passed = result["numPassedTests"].as_i64().unwrap();
+    let total = result["numTotalTests"].as_i64().unwrap();
+    let color = match passed as f32 / total as f32 {
+        r if r < 0.33 => "red",
+        r if r < 0.66 => "orange",
+        r if r < 1.0 => "yellow",
+        _ => "brightgreen",
+    };
+
+    let shield = json!({
+        "schemaVersion": 1,
+        "label": "shellshot",
+        "message": format!("{} / {} tests", passed, total),
+        "color": color,
+        "isError": true
+    });
+    fs::create_dir_all("./target/shields").expect("failed to create ./target/shields folders");
+    fs::write(
+        "./target/shields/shellshot.json",
+        serde_json::to_string_pretty(&shield).expect("failed to generate shields endpoint"),
+    ).expect("failed to save shields endpoint");
 }
