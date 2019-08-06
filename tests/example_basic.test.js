375636e61e595a4e8fdd078d2364d008e7c8528b
Fin Christensen <christensen.fin@gmail.com>
2019-08-06 18:22:08 +0200

    Add snapshot test for basic example

--- tests/example_basic.test.js
+++ tests/example_basic.test.js
@@ -5,7 +5,12 @@ setup();
 it(
     'checks if the basic example produces the same output',
     async () => {
-        await expect.command('script -qc "cargo run --example basic" out || less out | sha1sum')
-          .forStdout(exp => exp.toMatchSnapshot());
+        await expect.command('timeout 1 script --quiet --command ../target/debug/examples/basic /dev/null')
+            .withEnv({
+                TERM: "xterm-256color",
+                LINES: 24,
+                COLUMNS: 80,
+            })
+            .forStdout(expectation => expectation.toMatchSnapshot());
     },
 );
