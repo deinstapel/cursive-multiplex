277183207f8669334119bca6e3145e9da90b5fac
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-08-12 16:34:40 +0200

    Use tmux-2.9a

--- tests/example_basic.test.js
+++ tests/example_basic.test.js
@@ -7,9 +7,9 @@ it(
     async () => {
         await expect.command('cargo build --example basic --verbose')
             .forExitCode(exp => exp.toBe(0));
-        await expect.command('tmux new-session -x 80 -y 24 -d ../target/debug/examples/basic \; set status off && sleep 1')
+        await expect.command('../tmux-2.9a/tmux new-session -x 80 -y 24 -d ../target/debug/examples/basic \; set status off && sleep 1')
             .forExitCode(exp => exp.toBe(0));
-        await expect.command('tmux capture-pane -J -p -t %0')
+        await expect.command('../tmux-2.9a/tmux capture-pane -J -p -t %0')
             .forStdout(exp => exp.toMatchSnapshot());
     },
 );
