2ad46cec775e55c203d8afc526da15a1bbd0c758
Fin Christensen <christensen.fin@gmail.com>
2019-08-12 14:09:48 +0200

    Add tmux -V to travis config

--- tests/example_basic.test.js
+++ tests/example_basic.test.js
@@ -9,7 +9,7 @@ it(
             .forExitCode(exp => exp.toBe(0));
         await expect.command('tmux new-session -x 80 -y 24 -d ../target/debug/examples/basic \; set status off && sleep 1')
             .forExitCode(exp => exp.toBe(0));
-        await expect.command('tmux capture-pane -J -p -t 0')
+        await expect.command('tmux capture-pane -J -p -t %0')
             .forStdout(exp => exp.toMatchSnapshot());
     },
 );
