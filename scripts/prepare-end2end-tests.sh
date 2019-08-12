f5a6ce13e27cba85df03cff28b0c6e70f19da142
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-08-12 16:28:57 +0200

    Add tmux-2.9a for tests

--- scripts/prepare-end2end-tests.sh
+++ scripts/prepare-end2end-tests.sh
@@ -13,4 +13,13 @@ die() {
     npm install \
         --no-package-lock \
         --loglevel error || die "failed to install end2end test dependencies"
+
+    # Install latest release tmux for special ubuntu
+    cd ".."
+    wget https://github.com/tmux/tmux/releases/download/2.9a/tmux-2.9a.tar.gz
+    tar -xf tmux-2.9a.tar.gz
+    cd "tmux-2.9a" || die "folder not found: ./tmux-2.9a"
+    ./configure && make
+    cd ..
+    rm tmux-2.9a.tar.gz
 )
