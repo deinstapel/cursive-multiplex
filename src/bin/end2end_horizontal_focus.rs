bb250c81ff2f417fb87e5d9a7c3b96b55894bc20
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-08-15 18:16:41 +0200

    Fix test

--- src/bin/end2end_horizontal_focus.rs
+++ src/bin/end2end_horizontal_focus.rs
@@ -7,8 +7,8 @@ use cursive_multiplex::Mux;
 fn main() {
     let mut siv = Cursive::default();
     let (mut mux, left) = Mux::new(TextArea::new());
-    let right = mux.add_horizontal_id(TextArea::new(), left).expect("right failed");
-    let _id = mux.add_horizontal_id(TextArea::new(), left).expect("1st failed");
+    let right = mux.add_right_of(TextArea::new(), left).expect("right failed");
+    let _id = mux.add_right_of(TextArea::new(), left).expect("1st failed");
     mux.set_focus(right);
 
     siv.add_fullscreen_layer(mux);
